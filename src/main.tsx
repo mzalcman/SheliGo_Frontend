24530f79fe8bcaa2b71129fd5aa0548f98afaecf
mzalcman <49089410@est.ort.edu.ar>
2026-05-26 09:03:20 -0300

    insertando tipografias

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./contexts/auth_context";
+import "./styles/fonts.css";
+import "./index.css";
 
 ReactDOM.createRoot(
   document.getElementById("root")!

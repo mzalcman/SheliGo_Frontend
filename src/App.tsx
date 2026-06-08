e0c2190c23065af31eefec5eb1d5c6f618c6a508
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2026-06-08 10:41:40 -0300

    pagina principal empezada anda mal la foto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import "./app.css";
 import HomePage from "./pages/home_page/home_page";
 import PublicationDetailPage from "./pages/publication_detail_page/publication_detail_page";
+import LandingPage from "./pages/landing_page/landing_page";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
-  // TEMPORAL:
-  // Elegimos qué pantalla probar
-
   return (
 
     <BrowserRouter>
+
       <Routes>
+
         <Route
           path="/"
+          element={<LandingPage />}
+        />
+
+        <Route
+          path="/home"
           element={<HomePage />}
         />
+
         <Route
           path="/publicacion/:id"
           element={<PublicationDetailPage />}
         />
+
       </Routes>
+
     </BrowserRouter>
 
   );
---
+++ src/pages/landing_page/landing_page.tsx
@@ -0,0 +1,51 @@
+import "./landing_page.css";
+
+const LandingPage = () => {
+
+  return (
+
+    <main className="landing_page">
+
+      <div className="landing_image_container" />
+
+      <section className="landing_content">
+
+        <h1 className="landing_logo">
+          SheliGo
+        </h1>
+
+        <h2 className="landing_title">
+          Lo que
+          <br />
+          perdiste
+          <br />
+          <span>
+            puede volver.
+          </span>
+        </h2>
+
+        <p className="landing_subtitle">
+          Reportá, buscá y recuperá en tu institución.
+        </p>
+
+        <div className="landing_buttons">
+
+          <button className="landing_register_button">
+            Registrarse
+          </button>
+
+          <button className="landing_login_button">
+            Iniciar Sesión
+          </button>
+
+        </div>
+
+      </section>
+
+    </main>
+
+  );
+
+};
+
+export default LandingPage;

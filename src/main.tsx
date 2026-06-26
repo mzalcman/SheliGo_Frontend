92f0f705dafc714a25cf8566a5dbb9ba7e1558e9
MICAELA BERMAN KRESERMAN <49550540@ort>
2026-06-26 08:39:29 -0300

    fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/fonts.css"; //agregado
 import "./index.css";
 import { AuthProvider } from "./contexts/auth_context";
 import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(
   document.getElementById("root")!
